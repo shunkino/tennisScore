da22fa381025c77ebbbf838dc35311f11681a9b5
shunkin <shunkin@sfc.wide.ad.jp>
2015-03-18 22:12:16 +0900

    not working yet

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
+var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var ECT = require('ect'); //add ect
@@ -9,9 +10,25 @@ var ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext : '.ect' })
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var tennis = require('./models/tennis');
+var MongoStore = require('connect-mongo')(session);
 
 var app = express();
-
+//about cookie
+app.use(cookieParser());
+app.use(session({
+	saveUninitialized: true,
+	resave: true,
+	secret: 'himitsu',
+	store: new MongoStore({
+		db: 'tennis',
+		host: 'localhost',
+		clear_interval: 60 * 60
+	}),
+	cookie: {
+		httpOnly: false,
+		maxAge: 60 * 60 * 1000
+	}		
+}));
 // view engine setup
 app.engine('ect', ectRenderer.render); //modified ect
 app.set('view engine', 'ect'); //modified ect
@@ -23,11 +40,24 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req,res) {
-	res.redirect('tennis/results');
-});
+//認証用関数
+var loginCheck = function(req, res, next) {
+	if(req.session.user) {
+		next();
+	}else {
+		res.redirect('/login');
+	}
+}
 
-app.get('tennis/result', function(req, res) {
+app.get('/', function(req, res) {
+	res.render('login', {title:"LOGIN"});
+});
+app.get('/login', routes.login);
+app.post('/add', routes.add);
+app.get('/after', loginCheck, function(req, res) {
+	res.render('login', {title: "login 成功！"});
+});
+app.get('/tennis/result', function(req, res) {
 	tennis.find({}, function(err, results) {
 		if(err) throw err;
 		res.render('list', {title:'Tennis Result Screen', results:results});
@@ -35,14 +65,12 @@ app.get('tennis/result', function(req, res) {
 });
 //app.use('/', routes);
 app.use('/users', users);
-
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-
 /// error handlers
 
 // development error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/tennis');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express4ect' });
-});
+exports.add = function(req, res) {
+	var newUser = new User(req.body);
+	newUser.save(function(err) {
+		if(err) {
+			console.log(err);
+			res.redirect('back');
+		}else {
+			res.redirect('/');
+		}
+	});
+}
 
-module.exports = router;
+exports.login = function(req, res) {
+	var identification = req.query.ID;
+	var password = req.query.password;
+	var query = { "ID": identification, "password": password }
+	User.find(query, function(err, data) {
+		if(err) {
+			console.log(err);
+		}
+		if(data == "") {
+			res.render('login');
+		}else {
+			req.session.user = identification;
+			res.redirect('/');
+		}
+	});
+}
